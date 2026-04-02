f8207dac17706b96bf02d1f40c7d8e014f7390f5
Matti Luukkainen <mluukkai@iki.fi>
2026-04-02 21:53:27 +0300

    part6-1

---
+++ src/App.jsx
@@ -0,0 +1,33 @@
+import { useNotes, useNoteActions } from './store'
+
+const App = () => {
+  const notes = useNotes()
+  const { add } = useNoteActions()
+
+  const generateId = () => Number((Math.random() * 1000000).toFixed(0))
+
+  const addNote = (e) => {
+    e.preventDefault()
+    const content = e.target.note.value
+    add({ id: generateId(), content, important: false })
+    e.target.reset()
+  }
+
+  return (
+    <div>
+      <form onSubmit={addNote}>
+        <input name="note" />
+        <button type="submit">add</button>
+      </form>
+      <ul>
+        {notes.map(note => (
+          <li key={note.id}>
+            {note.content} <strong>{note.important ? 'important' : ''}</strong>
+          </li>
+        ))}
+      </ul>
+    </div>
+  )
+}
+
+export default App
