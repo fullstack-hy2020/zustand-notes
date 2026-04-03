c1a5b64d0b53499d52bbc1400fb67317693bb69e
Matti Luukkainen <mluukkai@iki.fi>
2026-04-03 10:29:16 +0300

    part6-2

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,11 @@
-import { useNotes, useNoteActions } from './store'
+import NoteForm from './NoteForm'
+import NoteList from './NoteList'
 
-const App = () => {
-  const notes = useNotes()
-  const { add } = useNoteActions()
-
-  const generateId = () => Number((Math.random() * 1000000).toFixed(0))
-
-  const addNote = (e) => {
-    e.preventDefault()
-    const content = e.target.note.value
-    add({ id: generateId(), content, important: false })
-    e.target.reset()
-  }
-
-  return (
-    <div>
-      <form onSubmit={addNote}>
-        <input name="note" />
-        <button type="submit">add</button>
-      </form>
-      <ul>
-        {notes.map(note => (
-          <li key={note.id}>
-            {note.content} <strong>{note.important ? 'important' : ''}</strong>
-          </li>
-        ))}
-      </ul>
-    </div>
-  )
-}
+const App = () => (
+  <div>
+    <NoteForm />
+    <NoteList />
+  </div>
+)
 
 export default App
--- src/store.js
+++ src/store.js
@@ -1,10 +1,32 @@
 import { create } from 'zustand'
 
 const useNoteStore = create((set) => ({
-  notes: [],
+  notes: [
+    {
+      id: 1,
+      content: 'Zustand is less complex than Redux',
+      important: true,
+    }, {
+      id: 2,
+      content: 'React app benefits from custom hooks',
+      important: false,
+    }, {
+      id: 3,
+      content: 'Remember to sleep well',
+      important: true,
+    }
+
+  ],
   actions: {
     add: note => set(
-      state => ({ notes: [...state.notes, note] })
+      state => ({ notes: state.notes.concat(note) })
+    ),
+    toggleImportance: id => set(
+      state => ({
+        notes: state.notes.map(note =>
+          note.id === id ? { ...note, important: !note.important } : note
+        )
+      })
     )
   }
 }))
